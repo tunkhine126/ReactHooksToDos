46ea663119c63d4253637396719f81a5d6d8fa1b
tunkhine126 <tunkhine126@gmail.com>
2019-11-27 14:27:03 -0500

    initial useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todos from './todos'
 
 function App() {
+  // The first parameter, todos is the name of our state.
+  // The second parameter, setTodos is what we are going to use to set the state.
+  // The hook of useState is what React uses to "hook" into the state or lifecycle 
+  // of the component. The array of objects is the beginnings of our state.
+  const [ todos, setTodos ] = React.useState([
+    { text: "Learn React Hooks "},
+    { text: "Land at New Story" },
+    { text: "Build a todo app with hooks"}
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todos
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+        </div>
     </div>
   );
 }
