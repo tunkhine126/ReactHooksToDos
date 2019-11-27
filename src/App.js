f481999fdf19bba20cc0e4afbd21552cca8310ca
tunkhine126 <tunkhine126@gmail.com>
2019-11-27 16:19:02 -0500

    todo form added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import Todos from './todos'
+import TodoForm from './TodoForm'
 
 function App() {
   // The first parameter, todos is the name of our state.
   // The second parameter, setTodos is what we are going to use to set the state.
   // The hook of useState is what React uses to "hook" into the state or lifecycle 
   // of the component. The array of objects is the beginnings of our state.
-  const [ todos, setTodos ] = React.useState([
+  const [ todos, setTodos ] = useState([
     { text: "Learn React Hooks "},
     { text: "Land at New Story" },
     { text: "Build a todo app with hooks"}
   ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
 
   return (
     <div className="App">
@@ -24,6 +29,7 @@ function App() {
             todo={todo}
           />
         ))}
+        <TodoForm addTodo={addTodo}/>
         </div>
     </div>
   );
