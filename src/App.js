d1b64420f1da7252c5b25361347d4653e620a25b
tunkhine126 <tunkhine126@gmail.com>
2019-12-15 14:09:58 -0500

    Hooks form and state completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import React, { useState } from 'react';
 import './App.css';
-import Todos from './todos'
-import TodoForm from './TodoForm'
+
+const Todo = ({ todo }) => <div className="todo">{todo.text}</div>;
+
+function TodoForm({ addTodo }) {
+  // Setting state
+  const [value, setValue] = useState("");
+  // The first is the "value" and the second is how we are going to be setting the state. 
+  // The state starts off empty and as we add things to our state, it will add it to our list of to-do items.
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <a> Enter new Todo item: </a>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
 
 function App() {
-  // The first parameter, todos is the name of our state.
-  // The second parameter, setTodos is what we are going to use to set the state.
-  // The hook of useState is what React uses to "hook" into the state or lifecycle 
-  // of the component. The array of objects is the beginnings of our state.
+
+  // SETTING STATE:
   const [ todos, setTodos ] = useState([
+    // The first parameter, todos is the name of our state.
+    // The second parameter, setTodos is what we are going to use to set the state.
+
+    // The hook of useState is what React uses to "hook" into the state or lifecycle 
+    // of the component.
+
+    // The array of objects is the beginnings of our state.
     { text: "Learn React Hooks "},
     { text: "Land at New Story" },
     { text: "Build a todo app with hooks"}
@@ -22,15 +53,15 @@ function App() {
   return (
     <div className="App">
       <div className="todo-list">
+        <TodoForm addTodo={addTodo} />
         {todos.map((todo, index) => (
-          <Todos
+          <Todo
             key={index}
             index={index}
             todo={todo}
           />
         ))}
-        <TodoForm addTodo={addTodo}/>
-        </div>
+      </div>
     </div>
   );
 }
--- src/todos.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const Todo = ({ todo }) => <div className="todo">{todo.text}</div>;
-
-export default Todo;
